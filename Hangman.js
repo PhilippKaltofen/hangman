5bf7c00b8c27ab9b6434546191baec080aa57ba4
PhilippKaltofen <pkaltofen@yahoo.com>
2016-11-26 19:16:12 +0100

    start screen added
    added start screen, button to start game, animation at start screen

--- Hangman.js
+++ Hangman.js
@@ -1,3 +1,10 @@
+// Hangman Game 
+// by Philipp Kaltofen
+
+// --------- --------- --------- --------- ---------
+//        --------- SURROUNDINGS ---------
+// --------- --------- --------- --------- ---------
+
 // fire stuff
 
 var fireParticle = function(sPosX, sPosY) {
@@ -94,20 +101,39 @@ var hangingStation = function(){
     rect(350, 400, 10, 50);
     // ground
     rect(150, 400, 200, 10);
-    //stairs
+    // stairs
     rect(120, 440, 40, 10);
     rect(130, 420, 20, 20);
     rect(140, 400, 10, 20);
+    // box
+    rect(240, 385, 20, 15);
     //          2 -- sideStands
     rect(180, 300, 20, 100);
     rect(300, 300, 20, 100);
     //          3 -- top
     rect(180, 300, 120, 10);
+    
+    // color change
+    fill(128, 126, 114);
+    
+    //          4 -- rope
+    rect(248, 310, 4, 25);
+    
+    //          5 -- body || HEAD
+    ellipse(250, 340, 15, 15);
+    //          6 -- body || UPPER BODY
+    ellipse(250, 360, 15, 30);
+    //          7 -- body || LEG LEFT
+    rect(245, 370, 5, 15);
+    //          8 -- body || LEG RIGHT
+    rect(251, 370, 5, 15);
+    
 };
 
 // draw all object
 
 var drawNonAnimatedObjects = function(){
+    rectMode(LEFT);
     sun(250,420,200);
     ground(420);
     torch(50, 405);
@@ -115,30 +141,110 @@ var drawNonAnimatedObjects = function(){
     hangingStation();
 };
 
+// --------- --------- --------- --------- ---------
+// --------- ACTUAL GAME LOGIC STARTS HERE ---------
+// --------- --------- --------- --------- ---------
+
+// Game states
+// 0 -- Start
+// 1 -- In Game
+// 2 -- Winscreen
+// 3 -- Gameover
+
+var gameState = 0;
+
+// Start Screen
+
+var r1_posX = -220;
+var r2_posX = 720;
+
+var buttonColor = 220;
+var b_posX = 250;
+var b_posY = 450;
+
+var drawStartScreen = function(){
+    // TITEL
+    noStroke();
+    rectMode(CENTER);
+    background(230, 230, 230);
+    fill(40,40,40);
+    textAlign(CENTER);
+    textSize(50);
+    text("Hangman", 250, 200);
+    rect(r1_posX, 220, 220, 1);
+    rect(r2_posX, 150, 220, 1);
+    // BUTTON
+    fill(buttonColor);
+    rect(b_posX, b_posY, 200, 50);
+    fill(100);
+    textSize(20);
+    text("New Game", b_posX, b_posY + 8);
+};
+
+var mouseMoved = function(){
+    // hightlight start button
+    if(gameState === 0){
+        if((mouseX > b_posX - 100 && mouseX < b_posX + 100) && (mouseY > b_posY - 25 && mouseY < b_posY+25)){
+            buttonColor = 255;
+        } else {
+            buttonColor = 220;
+        }
+    }
+};
+
+var mousePressed = function(){
+    // new game from start button
+    if(gameState === 0){
+        if((mouseX > b_posX - 100 && mouseX < b_posX + 100) && (mouseY > b_posY - 25 && mouseY < b_posY+25)){
+            gameState = 1;
+        }
+    }
+};
+
 // draw function
 
 var draw = function() {
     frameRate(60);
-    background(198, 226, 247);
-    for(var i = 0; i < psSize; i++){
-        // left
-        particleSystem[i].drawParticle();
-        particleSystem[i].moveParticle();
-        if(i === psSize-1){
-            particleSystem.splice(1, 0, new fireParticle(50,400));
-            particleSystem.pop();
-        }
-        // right
-        particleSystem2[i].drawParticle();
-        particleSystem2[i].moveParticle();
-        if(i === psSize-1){
-            particleSystem2.splice(1, 0, new fireParticle(450,400));
-            particleSystem2.pop();
+    
+    // Start
+    if(gameState === 0){
+        drawStartScreen();
+        if(r1_posX <= 249 && r2_posX >= 249){
+            r1_posX += 5;
+            r2_posX -= 5;
         }
     }
+    // In Game
+    if(gameState === 1){
+        background(198, 226, 247);
+        for(var i = 0; i < psSize; i++){
+            // left
+            particleSystem[i].drawParticle();
+            particleSystem[i].moveParticle();
+            if(i === psSize-1){
+                particleSystem.splice(1, 0, new fireParticle(50,400));
+                particleSystem.pop();
+            }
+            // right
+            particleSystem2[i].drawParticle();
+            particleSystem2[i].moveParticle();
+            if(i === psSize-1){
+                particleSystem2.splice(1, 0, new fireParticle(450,400));
+                particleSystem2.pop();
+            }
+        }
+        
+        drawNonAnimatedObjects();
+    }
+    // Winscreen
+    if(gameState === 2){
+    }
+    // GameOver
+    if(gameState === 3){
+    }
     
-    drawNonAnimatedObjects();
-    
+    /* DEBUG OUTPUT
     fill(255,255,255);
-    text("active particles: " + particleSystem.length, 10, 20);
+    text("active particles: " + particleSystem.length + " - " + particleSystem2.length, 10, 20);
+    */
 };
